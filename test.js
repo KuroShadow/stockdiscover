b03525575667a04d9c7ab16cb8413be39938f154
shantini west <shantini.sw@gmail.com>
2016-11-09 09:31:45 +0800

    completed able to retrive all stocks

--- test.js
+++ test.js
@@ -1,12 +1,24 @@
-var rest = require('restler');
 
+var rest = require('restler');
+var MongoClient = require('mongodb').MongoClient;
+var Server = require('mongodb').Server;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost/stockdiscover';
+var Db = require('mongodb').Db;
+
+var dbx = new Db('stockdiscover', new Server('localhost',27017));
+var db = dbx.open();
 discover_stock();
 return;
 
 function discover_stock()
 {
+  var ttw_add = 10000;
+  var ttw = ttw_add;
   for(var i = "A".charCodeAt(0);i <= "Z".charCodeAt(0);i++) {
-  	retrieve_stock_letter(String.fromCharCode(i));
+  	setTimeout(retrieve_stock_letter,ttw,String.fromCharCode(i));
+    ttw = ttw + ttw_add;
   }
 
 
@@ -14,7 +26,7 @@ function discover_stock()
 
 function retrieve_stock_letter(theletter)
 {
-
+setTimeout(retrieve_stock_letter,1000*10*60,theletter);
 console.log("Processing " + theletter);
 rest.get('http://dev.markitondemand.com/Api/v2/Lookup?Input="' + theletter + '"').on('complete', function(result) {
   if (result instanceof Error) {
@@ -30,6 +42,7 @@ rest.get('http://dev.markitondemand.com/Api/v2/Lookup?Input="' + theletter + '"'
 function process_result(theresult)
 {
   console.log("Processing Result");
+  console.log(theresult)
   cnt = theresult.LookupResultList.LookupResult.length;
   console.log(cnt);
    for(i = 0;i < cnt;i++){
@@ -39,10 +52,17 @@ function process_result(theresult)
 }
 function parse_stock_lookup(thestock)
 {
-      console.log(thestock)
-      console.log(thestock.Name)
-      console.log(thestock.Exchange)
-      console.log(thestock.Symbol)
 
+      thekey = thestock.Exchange + "-" + thestock.Symbol;
+      existing = dbx.collection('stock').find( {key: thekey} );
+      console.log('Existing ' + existing.count)
+      if (existing.count > 0){
+        console(console.log("Duplicate " + thekey));
+        return;
+      }
+      console.log("Adding " + thekey);
+      var document = {key:thekey, symbol:thestock.Symbol, name:thestock.Name, exchange:thestock.Exchange };
+      dbx.collection('stock').insert(document, {w: 1}, function(err, records){
+});
 
 }
--- test2.js
+++
@@ -1,9 +0,0 @@
-
-var  number = 0;
-while (number  <= 26) {
-  console.log(number);
-number = number + 1;
-}
-//!0
-//!2
-// ...    etcetera
